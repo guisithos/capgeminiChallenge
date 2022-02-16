809635331f9831d0f704aa1280a074dd9b5399c3
Guilherme Thomas <gui.thomas@outlook.com>
2022-02-16 16:06:47 -0300

    Update ladder.java

--- Questao1/ladder.java
+++ Questao1/ladder.java
@@ -1,22 +1,36 @@
+/**
+ * @author Guilherme Thomas
+ * Desafio de Programacao Capgemini
+ * Questao 01
+ */
 import java.util.Scanner;
 
-public class ladder {
-    
-        /**
-     * @param args /**
-     * int ladderLenght será nosso input para calcular o numero de
-     * degraus. |
-     * int i, j serao linhas e colunas respectivamente.
+public class criarEscada {
+
+    /**
+     * <b>Método usado para ler um input e criar uma
+     * escada com o numero digitado.</b> <br>
+     *
+     * Usamos a classe Scanner para ler o input do usuario
+     * Armazenamos o input em "degraus" <br>
+     *
+     * "i" serao as linhas e "j" as colunas
+     * Um loop cuida do asterisco e outro dos espacos <br>
+     *
+     * P.ex: Input 2, pra criar 2 degraus precisamos
+     * de um asterisco na primeira linha e
+     * um espaco em branco e um asterisco na segunda linha
      */
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Vamos construir uma escada, digite o número de degraus: ");
-        int ladderLenght = scanner.nextInt();
+        System.out.print("Vamos constSruir uma escada, digite o numero de degraus: ");
+        int degraus = scanner.nextInt();
 
         int i, j;
-        for (i=0; i<ladderLenght; i++) {
-            for (j=2*(ladderLenght-i); j>=0; j--) {
+        for (i=0; i< degraus; i++) {
+            for (j=2*(degraus -i); j>=0; j--) {
                 System.out.print(" ");
             }
             for (j=0; j<=i; j++ ) {
