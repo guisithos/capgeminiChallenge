025d9cc5e1c0dc9be5ba0edac352aab461affa71
Guilherme Thomas <gui.thomas@outlook.com>
2022-02-16 23:27:01 -0300

    Create verificaAnagrama.java

---
+++ Questao3/verificaAnagrama.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class verificaAnagrama {
+    public static void main(String[] args) {
+
+        //Definimos a palavra a checar entre " "
+        subString("ifailuhkqq");
+    }
+
+    static void subString(String palavra) {
+        //
+        HashMap< String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < palavra.length(); i++) {
+            for(int j = i; j < palavra.length(); j++) {
+                char[] valC = palavra.substring(i, j+1).toCharArray();
+                Arrays.sort(valC);
+                String val = new String(valC);
+
+                if (map.containsKey(val))
+                    map.put(val, map.get(val)+1);
+                else
+                    map.put(val, 1);
+            }
+        }
+        int contaPares = 0;
+        for (String key: map.keySet()) {
+            int n = map.get(key);
+            contaPares += (n * (n-1))/2;
+        }
+        System.out.println(contaPares);
+    }
+}
