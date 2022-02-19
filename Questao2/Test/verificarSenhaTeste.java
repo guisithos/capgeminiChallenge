3961f20a3869508483e2c19f538fa77c31f7d228
Guilherme Thomas <gui.thomas@outlook.com>
2022-02-19 16:33:29 -0300

    Create verificarSenhaTeste.java

---
+++ Questao2/Test/verificarSenhaTeste.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class verificarSenhaTest {
+    @Test
+    public void testeDigito() {
+        verificarSenha senha = new verificarSenha("kdkdk3");
+        assertEquals(testeDigito(););
+    }
+
+}
