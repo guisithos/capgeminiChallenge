b92b8f6cb9ccf1e9da90a9c89b7049fbef369bcc
Guilherme Thomas <gui.thomas@outlook.com>
2022-02-19 15:46:22 -0300

    Update criarEscadaTest.java

--- Questao1/Test/criarEscadaTest.java
+++ Questao1/Test/criarEscadaTest.java
@@ -1,5 +1,28 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class criarEscada {
+class criarEscadaTest {
+    @Test
+    void testeDegraus() {
+        List<String> degrausEsperado = new ArrayList<>();
+        degrausEsperado.add("      *");
+        degrausEsperado.add("     **");
+        degrausEsperado.add("    ***");
+        degrausEsperado.add("   ****");
+        degrausEsperado.add("  *****");
+        degrausEsperado.add(" ******");
+
+        List <String> degraus = new criarEscada(6);
+
+        assertNotNull(degraus);
+        assertEquals(3, degraus());
+
+
+    }
 
 }
